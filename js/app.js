be5ebe14bc57aa23a017d6c32f9c00af810b1ae1
Abby <52177245+AbiVickery@users.noreply.github.com>
2024-08-02 23:33:11 -0500

    decor
    snow in the background and an interactive cursor (only on pc)

--- js/app.js
+++ js/app.js
@@ -17,3 +17,18 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px");
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
